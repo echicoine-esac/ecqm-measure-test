e08abc56811a4db88003e6c5693c82206d03a444
Evan Chicoine <evan.chicoine@esacinc.com>
2022-09-09 16:53:03 -0400

    Code coverage on ServerUtils

--- src/tests/utils/ServerUtils.test.tsx
+++ src/tests/utils/ServerUtils.test.tsx
@@ -1,14 +1,70 @@
-import { Server } from '../../models/Server';
+import { GraphQLResult } from '@aws-amplify/api';
+import { GraphQLOptions } from '@aws-amplify/api-graphql/lib-esm/types/index';
+import { GraphQLError } from 'graphql/error/GraphQLError';
+import { API } from 'aws-amplify';
 import { ServerUtils } from '../../utils/ServerUtils';
 
-jest.mock('../../utils/ServerUtils')
+const mockCreateServerFn = jest.fn();
 
-jest.spyOn(ServerUtils, 'getServerList').mockImplementation(async () => {
-    return await ServerUtils.buildServerTestData();
+const mockGraphQlFn = jest.fn(async (): Promise<GraphQLResult<any>> => {
+    return new Promise<GraphQLResult<any>>((resolve, reject) => { });
+});
+
+it('ServerUtils fail on get server ', async () => {
+    const errMsg = 'Testing ServerUtils.getServerList error handling';
+
+    jest.spyOn(API, 'graphql').mockImplementation(async (options: GraphQLOptions, additionalHeaders?: { [key: string]: string; } | undefined): Promise<GraphQLResult<any>> => {
+        const graphQLError = new GraphQLError(errMsg);
+        const errorsList: GraphQLError[] = [graphQLError]
+        const graphQLErrors =  {
+            errors: errorsList
+        };
+        throw  graphQLErrors;
+    });
+
+    try {
+        await ServerUtils.getServerList();
+    } catch (error: any) {
+        expect(error.message).toContain(errMsg);
+    }
+
+});
+
+it('ServerUtils fail on create server ', async () => {
+    const errMsg = 'Testing ServerUtils.createServer error handling';
+
+    jest.spyOn(API, 'graphql').mockImplementation(async (options: GraphQLOptions, additionalHeaders?: { [key: string]: string; } | undefined): Promise<GraphQLResult<any>> => {
+        const graphQLError = new GraphQLError(errMsg);
+        const errorsList: GraphQLError[] = [graphQLError]
+        const graphQLErrors =  {
+            errors: errorsList
+        };
+        throw  graphQLErrors;
+    });
+
+    try {
+        await ServerUtils.createServer('baseUrl', 'authUrl', 'tokenUrl', 'clientId',
+            'clientSecret', 'scope');
+    } catch (error: any) {
+        expect(error.message).toContain(errMsg);
+    }
+});
+
+it('get server ', async () => {
+    jest.spyOn(API, 'graphql').mockImplementation(async (options: GraphQLOptions, additionalHeaders?: { [key: string]: string; } | undefined): Promise<GraphQLResult<any>> => {
+        return await mockGraphQlFn();
+    });
+
+    const serverList = await ServerUtils.getServerList();
+    expect(mockGraphQlFn).toHaveBeenCalled();
 });
 
 it('success example', async () => {
-    const serverList = await ServerUtils.getServerList();
-    expect(serverList).toEqual(ServerUtils.buildServerTestData());
+    jest.spyOn(API, 'graphql').mockImplementation(async (options: GraphQLOptions, additionalHeaders?: { [key: string]: string; } | undefined): Promise<GraphQLResult<any>> => {
+        return await mockGraphQlFn();
+    });
+
+    await ServerUtils.createServer('baseUrl', 'authUrl', 'tokenUrl', 'clientId', 'clientSecret', 'scope')
+    expect(mockGraphQlFn).toHaveBeenCalled();
 });
 
