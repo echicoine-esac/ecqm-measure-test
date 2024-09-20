6862940140cf332d7af4eaa6cf4a8e0d20695fa6
Evan Chicoine <evan.chicoine@esacinc.com>
2024-09-20 17:03:15 -0400

    Restoring version

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -9,17 +9,11 @@ interface props {
 // Results component displays the status messages
 const Results: React.FC<props> = ({ results }) => {
   return (
-    results.length > 0 ?
-      (<div className='row mt-4' >
-        <div className='col-md-12 order-md-1'>
-          <Form.Control data-testid='results-text' as='textarea' name='results' rows={20} value={results} readOnly />
-        </div>
-      </div >)
-
-      :
-
-      (<div></div>)
-
+    <div className='row mt-4' >
+      <div className='col-md-12 order-md-1'>
+        <Form.Control data-testid='results-text' as='textarea' name='results' rows={20} value={results} readOnly />
+      </div>
+    </div >
   );
 };
 
