d0615cbfb438eb7813f74713439283090802d89f
Evan Chicoine <evan.chicoine@esacinc.com>
2024-10-04 18:01:50 -0400

    Correcting the json data returned by -data so it can be used in -data

--- src/data/CollectDataFetch.tsx
+++ src/data/CollectDataFetch.tsx
@@ -93,11 +93,57 @@ export class CollectDataFetch extends AbstractDataFetch {
 
     protected processReturnedData(data: any) {
         try {
-            const ret: string = JSON.stringify(data, undefined, 2)
+            const ret: string = this.makeJsonDataSubmittable(JSON.stringify(data, undefined, 2));
             return ret;
         } catch (error: any) {
             return data;
         }
     }
 
+    /**
+     * Current bugs in hapi-fhir: 
+     * - Data returned using $collect-data associates ids in the name entry for each resource. These names are used in 
+     *   SubmitDataProvider as 'OperationParam' identifiers, which are validating by whole string only, which means 
+     *   'measureReport-1234' will not be found but 'measureReport' will.
+     * 
+     *   Example:   "name": "measureReport-e8029124-d760-40eb-b25a-703e447a3e4d"
+     *               will convert to
+     *              "name": "measureReport"
+     * 
+     * - Measure identification in the data returned by $collect-data includes version.
+     *      
+     *   Example:   "measure": "https://madie.cms.gov/Measure/AlaraCTClinicalFHIR|0.4.000"
+     *              will convert to
+     *              "measure": "https://madie.cms.gov/Measure/AlaraCTClinicalFHIR"
+     * @param jsonString 
+     * @returns 
+     */
+    private makeJsonDataSubmittable(jsonString: string): string {
+        let jsonData = JSON.parse(jsonString);
+        if (jsonData.parameter && Array.isArray(jsonData.parameter)) {
+            jsonData.parameter.forEach((entry: any) => {
+
+                //strip the id in the name field:
+                if (entry.name && typeof entry.name === 'string') {
+                    if (entry.name.startsWith('measureReport-')) {
+                        entry.name = 'measureReport';
+                    }
+                    if (entry.name.startsWith('resource-')) {
+                        entry.name = 'resource';
+                    }
+                }
+
+                //strip measure version if present:
+                if (entry.resource && entry.resource.resourceType === 'Measure') {
+                    if (entry.resource.id && typeof entry.resource.id === 'string') {
+                        const measureIdParts = entry.resource.id.split('|');
+                        if (measureIdParts.length > 1) {
+                            entry.resource.id = measureIdParts[0];
+                        }
+                    }
+                }
+            });
+        }
+        return JSON.stringify(jsonData, null, 2);
+    }
 }
