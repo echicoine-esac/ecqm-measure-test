cff976c497ba7512492fc304c1a09e8c254d42c2
Evan Chicoine <evan.chicoine@esacinc.com>
2024-10-05 08:39:48 -0400

    organized imports

--- src/components/SectionalResults.tsx
+++ src/components/SectionalResults.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Constants } from '../constants/Constants';
 
 // Props for Results panel
